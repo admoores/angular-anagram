6adf466ddd951af19ddcc600aab16c64ab56c474
Alex Moores <moores.alexd@gmail.com>
2017-09-25 12:54:37 -0400

    Serve basic requirements

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ An anagram generator made using AngularJS
 ### Steps
 - Step 1: AngularJS Boilerplate
   - Used starting point from my own collection of home-made boilerplates, in this case AngularJS+Express.
+- Step 2: Basic Requirements
+  - Create string shuffle function, shuffle text on ng-change or click of 'Re-Roll' button.
--- client/js/controllers/main.js
+++ client/js/controllers/main.js
@@ -2,4 +2,18 @@ angular.module('angular-anagram.main', [])
 
 .controller('MainController', function($scope) {
 
+  var shuffleString = function(string) {
+    var newStr = string.split('');
+    for (var i = string.length - 1; i > 0; i--) {
+      var randIndex = Math.floor(Math.random() * (i + 1));
+      var temp = newStr[randIndex];
+      newStr[randIndex] = newStr[i];
+      newStr[i] = temp;
+    }
+    return newStr.join('');
+  }
+
+  $scope.generateAnagram = function() {
+    $scope.outputText = shuffleString($scope.inputText);
+  }
 });
